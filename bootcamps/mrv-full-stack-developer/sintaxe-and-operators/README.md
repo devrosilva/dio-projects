fbe5d0fc29c0dd596cda05166e38cd7755169f31
devrosilva <rodrigosilva.info@gmail.com>
2021-12-21 00:00:59 -0300

    Sintaxe and Operators exercise

---
+++ bootcamps/mrv-full-stack-developer/sintaxe-and-operators/README.md
@@ -0,0 +1,14 @@
+# Sintaxe and Operators
+
+<p>This repository contains the final exercise to verify content learning of the module Sintaxe and Operators.</p>
+
+## Exercise
+
+- Create a function that receives two numbers as parameters;
+- Verify if the numbers are even;
+- Verify if their sum is higher than 10 or less than 20;
+- Return a string which says: The numbers num1 and num2 are/aren't equal. Their sum is (variable sum), which is greater/less than 10 and greater/less than 20.
+
+Ex.: 
+- Input: 1,2
+- Output: The numbers 1 and 2 are not equal. Their sum is 3, which is less than 10 and less than 20.
---
+++ bootcamps/mrv-full-stack-developer/sintaxe-and-operators/index.js
@@ -0,0 +1,14 @@
+//Exercise
+//Create a function that receives two numbers, does some comperason and shows the result.
+function compareNumbers(firstNumber, secondNumber){
+   
+    const isEven = firstNumber === secondNumber ? "are equal" : "are not equal";
+    const isLower = firstNumber + secondNumber > 10 ? "greater than" : "less than";
+    const isHigher = firstNumber + secondNumber > 20 ? "greater than" : "less than";
+
+    return `The numbers ${firstNumber} and ${secondNumber} ${isEven}. Their sum are ${firstNumber + secondNumber} which are ${isLower} 10 and ${isHigher} 20.`;
+}
+
+const randomNumber = () => Math.floor((Math.random() * 100)/5, 0);
+const result = compareNumbers(randomNumber(), randomNumber());
+console.log(result);
